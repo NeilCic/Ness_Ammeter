fb16dd23b62b31f89f1ba7338efd38d86bd3c605
NeilCic <50068396+NeilCic@users.noreply.github.com>
2026-09-24 15:05:19 +0300

    got ammeters working on main.py according to README file

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ from Ammeters.Entes_Ammeter import EntesAmmeter
 from Ammeters.Greenlee_Ammeter import GreenleeAmmeter
 from Ammeters.client import request_current_from_ammeter
 
+GREENLEE_PORT = 5000
+ENTES_PORT = 5001
+CIRCUTOR_PORT = 5002
 
 def run_greenlee_emulator():
-    greenlee = GreenleeAmmeter(5001)
+    greenlee = GreenleeAmmeter(GREENLEE_PORT)
     greenlee.start_server()
 
 def run_entes_emulator():
-    entes = EntesAmmeter(5002)
+    entes = EntesAmmeter(ENTES_PORT)
     entes.start_server()
 
 def run_circutor_emulator():
-    circutor = CircutorAmmeter(5003)
+    circutor = CircutorAmmeter(CIRCUTOR_PORT)
     circutor.start_server()
 
 if __name__ == "__main__":
@@ -25,13 +28,11 @@ if __name__ == "__main__":
     threading.Thread(target=run_entes_emulator, daemon=True).start()
     threading.Thread(target=run_circutor_emulator, daemon=True).start()
 
-    # This section is commented out because it shouldn't work.
-    # Read the README.md file as well as the source code if you need, and fix the problem.
-
     # Wait for the servers to start, if you have problem restarting the servers between runs try increasing sleep time.
     time.sleep(5)
-    request_current_from_ammeter(5001, b'MEASURE_GREENLEE')  # Request from Greenlee Ammeter
-    request_current_from_ammeter(5002, b'MEASURE_ENTES')  # Request from ENTES Ammeter
-    request_current_from_ammeter(5003, b'MEASURE_CIRCUTOR')  # Request from CIRCUTOR Ammeter
+
+    request_current_from_ammeter(GREENLEE_PORT, b'MEASURE_GREENLEE -get_measurement')  # Request from Greenlee Ammeter
+    request_current_from_ammeter(ENTES_PORT, b'MEASURE_ENTES -get_data')  # Request from ENTES Ammeter
+    request_current_from_ammeter(CIRCUTOR_PORT, b'MEASURE_CIRCUTOR -get_measurement')  # Request from CIRCUTOR Ammeter
 
     pass
